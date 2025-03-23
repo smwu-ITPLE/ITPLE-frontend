d224726128ee62b61fef3b7a7315da05c3831a11
Answl <118355919+Answl@users.noreply.github.com>
2025-03-24 00:26:53 +0900

    gitignore 파일 변동으로 인한 업로드

---
+++ sdk/build/intermediates/cxx/Debug/6tu6h131/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\keji1\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\keji1\\AndroidStudioProjects\\maeumgil\\sdk\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\keji1\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\keji1\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\keji1\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\keji1\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\keji1\\AndroidStudioProjects\\maeumgil\\sdk\\build\\intermediates\\cxx\\Debug\\6tu6h131\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\keji1\\AndroidStudioProjects\\maeumgil\\sdk\\build\\intermediates\\cxx\\Debug\\6tu6h131\\obj\\arm64-v8a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\keji1\\AndroidStudioProjects\\maeumgil\\sdk\\.cxx\\Debug\\6tu6h131\\arm64-v8a" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
